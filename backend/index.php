0ad44abe9fda62b4a603b451cd9908884a8bff3b
Gabriela Melo <gaabmmelo@gmail.com>
2024-05-18 00:06:24 -0300

    add routes of backend

--- backend/index.php
+++ backend/index.php
@@ -15,5 +15,8 @@ $router->options('/product', function() { /* ... */ });
 
 /*TIPO DE PRODUTO*/
 $router->get('/product_type', '\App\Controllers\ProductTypeController@index');
+$router->get('/product_type/{id}', '\App\Controllers\ProductController@show');
+$router->put('/product_type/{id}', 'ProductTypeController@update');
+$router->delete('/product_type/{id}', 'ProductTypeController@delete');
 
 $router->run();
