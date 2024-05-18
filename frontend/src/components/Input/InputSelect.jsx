43a89cda12e1c223f5d1e234038171f595ef3834
Gabriela Melo <gaabmmelo@gmail.com>
2024-05-18 13:47:35 -0300

    fron-end componente: inputSelect

---
+++ frontend/src/components/Input/InputSelect.jsx
@@ -0,0 +1,24 @@
+import { MenuItem, Select } from "@mui/material";
+import React from "react";
+
+export default function InputSelect({
+  labelId,
+  id,
+  value,
+  label,
+  onChange,
+  children,
+}) {
+  return (
+    <Select
+      fullWidth
+      labelId={labelId}
+      id={id}
+      value={value}
+      label={label}
+      onChange={onChange}
+    >
+      {children}
+    </Select>
+  );
+}
