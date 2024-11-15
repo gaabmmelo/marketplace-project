951981d9c5582d87abf3849f8a8a8366e41a33b7
Gabriela Melo <gaabmmelo@gmail.com>
2024-11-15 14:05:23 -0300

    front-end: update alert component with timer linear progress

--- frontend/src/components/Alert/Alert.jsx
+++ frontend/src/components/Alert/Alert.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import Snackbar from "@mui/material/Snackbar";
-import { Alert } from "@mui/material";
+import { Alert, LinearProgress } from "@mui/material";
 
 export default function AlertStyled() {
   const [open, setOpen] = useState(true);
+  const [progress, setProgress] = useState(100);
+  const [snackbarVisibility, setSnackbarVisibility] = useState(false);
 
   const handleClose = (event, reason) => {
     if (reason === "clickaway") {
@@ -13,21 +15,57 @@ export default function AlertStyled() {
     setOpen(false);
   };
 
+  const handleAction = () => {
+    setSnackbarVisibility(true);
+
+    const interval = setInterval(() => {
+      setProgress((prevProgress) => Math.max(prevProgress - 0.15, 0));
+    }, 10);
+
+    const duration = setTimeout(() => {
+      clearInterval(interval);
+      setSnackbarVisibility(false);
+      setProgress(0);
+    }, 4000);
+
+    return () => {
+      clearTimeout(duration);
+      clearInterval(interval);
+    };
+  };
+
   useEffect(() => {
-    const timer = setTimeout(() => setOpen(false), 3000);
+    const timer = setTimeout(() => setOpen(false), 800000);
+    handleAction();
     return () => clearTimeout(timer);
-  }, [open]);
+  }, []);
 
   return (
     <Snackbar open={open} onClose={handleClose}>
-      <Alert
-        onClose={handleClose}
-        severity="success"
-        variant="filled"
-        sx={{ width: "100%" }}
-      >
-        Tipo de produto adicionado com sucesso!
-      </Alert>
+      <div>
+        <Alert
+          onClose={handleClose}
+          severity="success"
+          variant="filled"
+          sx={{
+            width: "100%",
+            borderBottomLeftRadius: "0px 0px",
+            borderBottomRightRadius: "0px 0px",
+          }}
+        >
+          Tipo de produto adicionado com sucesso!
+        </Alert>
+        <LinearProgress
+          sx={{
+            width: "100%",
+            padding: "0px 16px",
+            borderBottomLeftRadius: "5px 5px",
+            borderBottomRightRadius: "5px 5px",
+          }}
+          variant="determinate"
+          value={progress}
+        />
+      </div>
     </Snackbar>
   );
 }
